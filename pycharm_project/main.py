0ae27f8d867b146e9d62feb1894a10d9b100348f
shecodessometimes <julia@bebopinteractive.com>
2022-04-12 17:21:02 -0400

    added ffmpeg library

--- pycharm_project/main.py
+++ pycharm_project/main.py
@@ -1,6 +1,8 @@
 from audio_control import AudioControl
+import ffmpeg
 
 print("Hello world!")
 a = AudioControl(440, 44100, 20, "a")
 a.convert_text_to_wav("Hello how are you today?!")
 
+#convert wav to mp4
