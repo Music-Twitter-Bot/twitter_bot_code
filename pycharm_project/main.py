a96ecea08b81735c6960a6f02a77a042a5a0535e
shecodessometimes <julia@bebopinteractive.com>
2022-04-12 18:29:15 -0400

    implemented ffmpy

--- pycharm_project/main.py
+++ pycharm_project/main.py
@@ -1,6 +1,6 @@
 import ffmpy
-
 from audio_control import AudioControl
+
 import ffmpeg
 from ffmpy import FFmpeg
 
@@ -11,6 +11,6 @@ a.convert_text_to_wav("Hello how are you today?!")
 #convert wav to mp4
 audio_file = "sound_full.wav"
 image_file = "image.jpg"
-ff = ffmpy.FFmpeg(executable='ffmpeg/bin/ffmpeg.exe', inputs={'sound_full.wav': None}, outputs={'video.mp4': ["-filter:a", "atempo=0.5"]})
+ff = ffmpy.FFmpeg(executable='ffmpeg/ffmpeg.exe', inputs={'sound_full.wav': None}, outputs={'video.mp4': ["-filter:a", "atempo=0.5"]})
 ff.run()
 
